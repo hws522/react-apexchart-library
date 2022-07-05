475c965b9c439f83a202924092ef82a9b377d3f8
hws522 <sraphic@kakao.com>
2022-07-05 22:46:19 +0900

    import BubbleChart.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import LineChart from './apexcharts/LineChart';
 import BarChart from './apexcharts/BarChart';
 import AreaChart from './apexcharts/AreaChart';
+import BubbleChart from './apexcharts/BubbleChart';
 
-export { LineChart, BarChart, AreaChart };
+export { LineChart, BarChart, AreaChart, BubbleChart };
