d9224ca212ecd5a7ed136a2ec1bb3930824d5ac2
hws522 <sraphic@kakao.com>
2022-07-13 22:47:56 +0900

    add basic heatmapChart

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import AreaChart from './apexcharts/AreaChart';
 import BubbleChart from './apexcharts/BubbleChart';
 import BoxPlotChart from './apexcharts/BoxPlotChart';
 import CandlestickChart from './apexcharts/CandlestickChart';
+import HeatmapChart from './apexcharts/HeatmapChart';
 
-export { LineChart, BarChart, AreaChart, BubbleChart, BoxPlotChart, CandlestickChart };
+export { LineChart, BarChart, AreaChart, BubbleChart, BoxPlotChart, CandlestickChart, HeatmapChart };
