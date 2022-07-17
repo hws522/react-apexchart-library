9ce760b36140fd04c9567e023104b5191474dd26
hws522 <sraphic@kakao.com>
2022-07-17 20:57:28 +0900

    add polar area chart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { LineChart, BarChart, BubbleChart, BoxPlotChart, CandlestickChart, HeatmapChart, PieChart } from './components';
+import { LineChart, BarChart, BubbleChart, BoxPlotChart, CandlestickChart, HeatmapChart, PieChart, PolarAreaChart } from './components';
 
 function App() {
   const chartSeries = [44, 55, 13, 43, 22];
-  const pieLabels = ['Label1', 'Label2', 'Label3', 'Label4', 'Label5'];
+  const polarLabels = ['Label1', 'Label2', 'Label3', 'Label4', 'Label5'];
 
   return (
     <>
-      <PieChart chartSeries={chartSeries} pieLabels={pieLabels} />
+      <PolarAreaChart chartSeries={chartSeries} polarLabels={polarLabels} dataLabelEnabled={false} />
     </>
   );
 }
